a8ac7a6f6a3c7ce75d2664cf3abc78f77ced2aaa
Simone Greci <s.greci@gmail.com>
2021-01-28 10:05:27 +0100

    Added module CentricoSecureSSISPackages

---
+++ CentricoSecureSSISPackages/CentricoSecureSSISPacakges.psm1
@@ -0,0 +1,11 @@
+function Run-CentricoSSISPackage {
+    param(
+        [String] $start = [DateTime]::Today,
+        [DateTime] $end = $start,
+        $firstDayOfWeek,
+        [int[]] $highlightDay,
+        [string[]] $highlightDate = [DateTime]::Today.ToString()
+        )
+    
+        #actual code for the function goes here see the end of the topic for the complete code sample
+    }
